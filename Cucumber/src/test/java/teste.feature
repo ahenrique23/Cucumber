c1d32960a4cfd324195f63781b0d74245b22f7ae
unknown <ailton.h.gomes@gmail.com>
2019-02-22 22:51:56 -0300

    Criação da classe runner

--- Cucumber/src/test/java/teste.feature
+++ Cucumber/src/test/java/teste.feature
@@ -1,10 +1,18 @@
 # language: pt
+
 Funcionalidade: Aprender cucumber
 	Como um aluno
 	Eu quero aprender a utilizar cucumber
-	Para que eu possa automatizar crit�rios de aceitação
+	Para que eu possa automatizar critérios de aceitação
 
 Cenario: Deve executar especificação	
 	Dado   que criei o arquivo corretamente
 	Quando executá-lo
-	Então  a especificacão deve finalizar com sucesso 
+	Então a especificacão deve finalizar com sucesso
+	
+Cenario: Contador
+	Dado que o valor do contador é 15
+	Quando incrementar 3
+	Então o valor do contador será 18
+		
+	
--- Cucumber/src/test/java/teste.java
+++ Cucumber/src/test/java/teste.java
@@ -5,14 +5,33 @@ import cucumber.api.java.pt.Quando;
 public class teste {
 
 	@Dado("^que criei o arquivo corretamente$")
-	public void que_criei_o_arquivo_corretamente() throws Throwable {
+	public void queCrieiOArquivoCorretamente() throws Throwable {
 	}
 
 	@Quando("^executá-lo$")
-	public void executá_lo() throws Throwable {
+	public void executáLo() throws Throwable {
 	}
 
 	@Então("^a especificacão deve finalizar com sucesso$")
-	public void a_especificacão_deve_finalizar_com_sucesso() throws Throwable {
+	public void aEspecificacãoDeveFinalizarComSucesso() throws Throwable {
 	}
+	
+	private int contador = 0;
+	
+	@Dado("^que o valor do contador é (\\d+)$")
+	public void queOValorDoContadorÉ(int arg1) throws Throwable {
+		contador = arg1;
+	}
+
+	@Quando("^incrementar (\\d+)$")
+	public void incrementar(int arg1) throws Throwable {
+		contador = contador + arg1;
+	}
+
+	@Então("^o valor do contador será (\\d+)$")
+	public void oValorDoContadorSerá(int arg1) throws Throwable {
+		System.out.println(arg1);
+		System.out.println(contador);
+	}
+	
 }
